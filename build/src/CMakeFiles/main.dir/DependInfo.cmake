9a45feb0815f2046b7107e9f220dcd79f04ee2de
dtertre59 <davidtertreboye@gmail.com>
2025-01-21 19:23:05 +0100

    setting up project

---
+++ build/src/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/dtertre59/Documents/Projects/Work_Projects/Material_Detector_Machine/control-system/src/main.cpp" "src/CMakeFiles/main.dir/main.cpp.o" "gcc" "src/CMakeFiles/main.dir/main.cpp.o.d"
+  "/Users/dtertre59/Documents/Projects/Work_Projects/Material_Detector_Machine/control-system/src/prueba.cpp" "src/CMakeFiles/main.dir/prueba.cpp.o" "gcc" "src/CMakeFiles/main.dir/prueba.cpp.o.d"
+  "/Users/dtertre59/Documents/Projects/Work_Projects/Material_Detector_Machine/control-system/src/prueba2.cpp" "src/CMakeFiles/main.dir/prueba2.cpp.o" "gcc" "src/CMakeFiles/main.dir/prueba2.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
