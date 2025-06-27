c838a454ab9df117794d4367a6750f73c1fdd124
David Tertre <davidtertreboye@gmail.com>
2025-06-27 19:04:47 +0100

    sh to run program

--- run_control_system.sh
+++ run_control_system.sh
@@ -2,6 +2,4 @@
 CONTROL_SYSTEM_PROJECT_PATH="/home/developer/Documents/material_detector_machine/control-system"
 
 # Run c++ executable
-"./$SENSOR_DETECTOR_PROJECT_PATH/main.py"
-
-deactivate
+"$CONTROL_SYSTEM_PROJECT_PATH/build/src/rpi_main"
